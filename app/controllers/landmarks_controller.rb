94227a14274df2a15d7a9964bda55d77b8eed8be
Ryan Hu <ryanhuuu@gmail.com>
2018-04-26 14:33:49 -0400

    finished title's views and controller, except delete

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,38 @@
+require 'pry'
 class LandmarksController < ApplicationController
+
+  get '/title' do
+    @titles = Title.all
+    erb:'titles/index'
+  end
+
+  post '/title' do
+    Title.find_or_create_by(name: params[:name])
+    @titles = Title.all
+    erb:"titles/index"
+  end
+
+  get '/title/new' do
+    erb:'titles/new'
+  end
+
+  get '/title/:id' do
+    @title = Title.find(params[:id])
+    erb:'titles/show'
+  end
+
+  patch '/title/:id' do
+    binding.pry
+    @title = Title.find(params[:id])
+    @title.name = params[:name]
+    @title.save
+    erb:'titles/show'
+  end
+
+  get '/title/:id/edit' do
+    @title = Title.find(params[:id])
+    erb:'titles/edit'
+  end
+
 
 end
