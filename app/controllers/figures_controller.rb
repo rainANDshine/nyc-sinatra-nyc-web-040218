9d06ed0b2d80ebae5d5685299663ae3810ab56ec
Ryan Hu <ryanhuuu@gmail.com>
2018-04-26 17:34:52 -0400

    finished everything

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -9,13 +9,13 @@ class FiguresController < ApplicationController
   get '/figures/new' do # render page to create a new figure
     @landmarks = Landmark.all
     @titles = Title.all
-
-
     erb :"figures/new"
   end
 
   get '/figures/:id' do # render page to display to create a figure
     @figure = Figure.find(params[:id])
+    @landmarks = Landmark.all
+    @titles = @figure.titles
     erb :"figures/show"
   end
 
@@ -26,6 +26,34 @@ class FiguresController < ApplicationController
 
   post '/figures' do # create a new figure
     figure = Figure.find_or_create_by(params[:figure])
+
+    #create title and add to fig
+    if params[:newTitle] != ""
+      tit = Title.create(name: params[:newTitle])
+      figure.titles << tit
+    end
+
+    #create landmark and assign to fig
+    if params[:newLandmark] != ""
+      Landmark.create(figure_id: figure.id, name: params[:newLandmark], year_completed: params[:yc].to_i)
+    end
+
+
+    #add all selected titles to fig
+    if params[:titles] != nil
+      params[:titles].each do |el|
+        a = Title.find(el)
+        figure.titles << a
+      end
+    end
+    #add all selected landmarks to fig
+    if params[:landmarks] != nil
+      params[:landmarks].each do |el|
+        a = Landmark.find(el)
+        a.figure_id = figure.id
+        a.save
+      end
+    end
     redirect to "figures/#{figure.id}"
   end
 
