b6c4d2afac4e3e793efe423c2a9fcdad3bbef4cb
github username <github email address>
2018-04-26 17:36:04 -0400

    some minor change, just ignore

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -1,39 +1,39 @@
-require 'pry'
-class TitlesController < ApplicationController
-
-  get '/titles' do
-    @titles = Title.all
-    erb:'titles/index'
-  end
-
-  post '/titles' do
-    title = Title.find_or_create_by(params[:title])
-    redirect to "titles/#{title.id}"
-  end
-
-  get '/titles/new' do
-    erb:'titles/new'
-  end
-
-  get '/titles/:id' do
-    @title = Title.find(params[:id])
-    erb:'titles/show'
-  end
-
-  patch '/titles/:id' do
-    title = Title.find(params[:id])
-    title.update(params[:title])
-    redirect to "titles/#{title.id}"
-  end
-
-  get '/titles/:id/edit' do
-    @title = Title.find(params[:id])
-    erb:'titles/edit'
-  end
-
-  delete '/titles/:id/delete' do # delete a figure
-    title = Title.find(params[:id])
-    title.delete
-    redirect to "titles"
-  end
-end
+# require 'pry'
+# class TitlesController < ApplicationController
+#
+#   get '/titles' do
+#     @titles = Title.all
+#     erb:'titles/index'
+#   end
+#
+#   post '/titles' do
+#     title = Title.find_or_create_by(params[:title])
+#     redirect to "titles/#{title.id}"
+#   end
+#
+#   get '/titles/new' do
+#     erb:'titles/new'
+#   end
+#
+#   get '/titles/:id' do
+#     @title = Title.find(params[:id])
+#     erb:'titles/show'
+#   end
+#
+#   patch '/titles/:id' do
+#     title = Title.find(params[:id])
+#     title.update(params[:title])
+#     redirect to "titles/#{title.id}"
+#   end
+#
+#   get '/titles/:id/edit' do
+#     @title = Title.find(params[:id])
+#     erb:'titles/edit'
+#   end
+#
+#   delete '/titles/:id/delete' do # delete a figure
+#     title = Title.find(params[:id])
+#     title.delete
+#     redirect to "titles"
+#   end
+# end
